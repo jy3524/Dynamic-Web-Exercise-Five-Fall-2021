cbd599e557e2fd435f387bcfa25abbaef5507875
Jeongin Yoon <jy3524@nyu.edu>
2021-11-16 08:24:41 -0500

    clean up

---
+++ src/components/CreateUserForm.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function CreateUserForm() {
+  return (
+    <div>
+      <h1></h1>
+    </div>
+  );
+}
+
+export default CreateUserForm;
---
+++ src/pages/Login.js
@@ -0,0 +1,12 @@
+import React from "react";
+import LoginForm from "../components/LoginForm";
+
+function Login() {
+  return (
+    <div>
+      <h1></h1>
+    </div>
+  );
+}
+
+export default Login;
