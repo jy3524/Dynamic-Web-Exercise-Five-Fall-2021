45f436582f823eec2d4874ee1ac512484a75429b
Jeongin Yoon <jy3524@nyu.edu>
2021-11-18 10:05:45 -0500

    setup firebase

--- src/App.js
+++ src/App.js
@@ -1,18 +1,58 @@
+import React, { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { initializeApp } from 'firebase/app';
+import { getAuth, onAuthStateChanged } from '@firebase/auth';
 import './App.css';
+// Page Imports
 import Header from './components/Header';
 import UserProfile from './pages/UserProfile';
 import CreateUser from './pages/CreateUser';
 import Login from './pages/Login';
+import firebaseConfig from "./components/FirebaseConfig"
 
 function App() {
+  // Track if user is logged in
+  const [loggedIn, setLoggedIn] = useState(false);
+  // Check to see if there is any loading...
+  const [loading, setLoading] = useState(true);
+  // Store user information in state
+  const [userInformation, setUserInformation] = useState({});
+  const [appInitialized, setAppInitialized] = useState(false);
+
+  // Initialize Firebase
+  useEffect(() => {
+    initializeApp(firebaseConfig);
+    setAppInitialized(true)
+  }, []);
+
+  useEffect(() => {
+    if (appInitialized) {
+      const auth = getAuth();
+      onAuthStateChanged(auth, (user) => {
+        if (user) {
+          // User is signed in,
+          setUserInformation(user);
+          setLoggedIn(true);
+        } else {
+          // User is singed out
+          setUserInformation({});
+          setLoggedIn(false);
+        }
+      });
+      setLoading(false)
+    }
+  }, [appInitialized]);
+
+  if (loading) return null;
+  console.log({userInformation, loggedIn});
+
   return (
     <>
       <Header />
       <Router>
         <Routes>
           <Route path="/user/:id" element={<UserProfile />} />
-          <Route path="/create" element={<CreateUser />} />
+          <Route path="/create" element={<CreateUser setLoggedIn={setLoggedIn} setUserInformation={setUserInformation} />} />
           <Route path="/" element={<Login />} />
         </Routes>
       </Router>
